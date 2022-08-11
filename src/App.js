4dc1e124f37c2f9949b1542f1b53b3ee70186e70
Ayon Mandal <ayonmandal@192.168.1.7>
2022-08-12 00:03:19 +0530

    Refactored Route Code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.scss';
+import "./App.scss";
+import AppRoutes from "./config/AppRoutes";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
-    <div className>
-      App
-    </div>
+    <BrowserRouter>
+      <Header />
+      <AppRoutes />
+      <Footer />
+    </BrowserRouter>
   );
 }
 

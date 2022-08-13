e222788c14fa288beebbeb7a80925fd183dc887d
Ayon Mandal <ayonmandal@192.168.1.3>
2022-08-13 20:39:58 +0530

    Header v1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../../assets/tmovie.png";
+import "./Header.scss";
+
+const headerNav = [
+  {
+    display: "Home",
+    path: "/",
+  },
+  {
+    display: "Movies",
+    path: "/movie",
+  },
+  {
+    display: "TV Series",
+    path: "/tv",
+  },
+];
 
 const Header = () => {
-  return <div>HEADER</div>;
+  const { pathname } = useLocation();
+  const headerRef = useRef(null);
+  const active = headerNav.findIndex((e) => e.path === pathname);
+
+  return (
+    <div ref={headerRef} className="header">
+      <div className="header__wrap container">
+        <div className="logo">
+          <img src={logo} alt="Tmovies Logo" />
+          <Link to="/">tMovies</Link>
+        </div>
+        <ul className="header__nav">
+          {headerNav.map((e, i) => (
+            <li key={i} className={`${i === active ? "active" : ""} `}>
+              <Link to={e.path}>{e.display}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;

9ffd7d3f8d0f86e322793bec8f92a96784b88540
Ayon Mandal <ayonmandal@192.168.1.3>
2022-08-14 16:09:51 +0530

    Button v1

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ReactPropTypes } from "react";
+import "./Button.scss";
+
+const Button = (props) => {
+  return (
+    <button
+      className={`btn ${props.className}`}
+      onClick={props.onClick ? () => props.onClick() : null}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+const OutlineButton = (props) => {
+  return (
+    <Button
+      className={`btn-outline ${props.className}`}
+      onClick={props.onClick ? () => props.onClick() : null}
+    >
+      {props.children}
+    </Button>
+  );
+};
+
+Button.ReactPropTypes = {
+  onClick: ReactPropTypes.func,
+};
+
+export default Button;

0580b786d085b358391923ee88e4520d9495fac8
Ayon Mandal <ayonmandal@192.168.1.3>
2022-08-14 19:38:05 +0530

    Modal completion v1

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,51 @@
+import { React, useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import "./Modal.scss";
+
+const Modal = (props) => {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    setActive(props.active);
+  }, [props.active]);
+
+  return (
+    <div id={props.id} className={`modal ${active ? "active" : ""}`}>
+      {props.children}
+    </div>
+  );
+};
+
+Modal.propType = {
+  active: PropTypes.bool,
+  id: PropTypes.string,
+};
+
+import React from "react";
+
+export const ModelContent = (props) => {
+  const contentRef = useRef(null);
+
+  const closeModal = () => {
+    contentRef.current.parentNode.classList.remove("active");
+    if (props.onClose) {
+      props.onClose();
+    }
+  };
+
+  return (
+    <div ref={contentRef} className="modal__content">
+      {props.children}
+      <div className="modal__content__close" onClick={closeModal}>
+        <i className="bx bx-x"></i>
+      </div>
+    </div>
+  );
+};
+
+ModelContent.propType = {
+  onClose: PropTypes.bool,
+};
+
+export default Modal;

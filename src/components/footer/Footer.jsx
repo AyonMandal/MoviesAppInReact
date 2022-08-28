559739839605ecb8952a309722fb4ac54d7cbadc
Ayon Mandal <ayonmandal@192.168.1.22>
2022-08-28 16:40:58 +0530

    Footer finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import backgroundImage from "../../assets/footer-bg.jpg";
+import logo from "../../assets/tmovie.png";
+import { Link } from "react-router-dom";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>FOOTER</div>;
+  return (
+    <>
+      <div
+        className="footer"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div className="footer_content">
+          <div className="footer_content_logo">
+            <div className="logo">
+              <img src={logo} alt="Tmovies Logo" />
+              <Link to="/">tMovies</Link>
+            </div>
+          </div>
+          <div className="footer_content_menus">
+            <div className="footer_content_menu">
+              <Link to="/">Home</Link>
+              <Link to="/">Contact us</Link>
+              <Link to="/">Term of services</Link>
+              <Link to="/">About us</Link>
+            </div>
+            <div className="footer_content_menu">
+              <Link to="/">Live</Link>
+              <Link to="/">FAQ</Link>
+              <Link to="/">Premium</Link>
+              <Link to="/">Privacy policy</Link>
+            </div>
+            <div className="footer_content_menu">
+              <Link to="/">Must watch</Link>
+              <Link to="/">Recent release</Link>
+              <Link to="/">Top IMDB</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;

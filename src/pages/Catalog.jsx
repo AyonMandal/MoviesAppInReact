9a150612465a961ba46b89c0f459d26506db6f46
Ayon Mandal <ayonmandal@192.168.1.5>
2022-08-31 18:10:44 +0530

    Page Header created for Catalog

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { category as Category } from "../api/tmdbApiConfig";
+import PageHeader from "../components/pageHeader/PageHeader";
 
 const Catalog = () => {
-  return <div>Catalog</div>;
+  const { category } = useParams();
+  console.log(category);
+  return (
+    <PageHeader>{category === Category.movie ? "movie" : "tv"}</PageHeader>
+  );
 };
 
 export default Catalog;
